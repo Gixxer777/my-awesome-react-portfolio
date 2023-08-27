a60c4efe1ce0dd109fc4f6a86f541e6c1a20023a
David Gaspard <dgaspard7777@gmail.com>
2023-08-26 19:44:12 -0500

    About Page

---
+++ src/components/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div id="aboutme">
+      <h1 >About Me</h1>
+      <p>
+        My name is David Gaspard and I am a newly minted full stack developer! I have a background in sales and the service industry, but I have always had
+        a passion for technology. With my background in sales and the service industry, I have a strong understanding of how to work with people. I would make
+        a great addition to any team and can generally fulfill a task if I am asked to do it! I have a can-do attitude, am very adaptable, and also very dependable,
+        which makes me an easy person to work with!
+      </p>
+      <p>
+        I have an Associates of Arts in Business from Lonestar Community College and am a recent graduate of the University of Texas Coding Bootcamp. With my degree in 
+        business I acquired skills in accounting, marketing, and management. Many of these skills are transferable to the tech industry and I am excited to see where 
+        my career takes me! I am currently looking for a full time position as a starting level full stack developer, but I am also open to taking on part time work or 
+        taking on an apprenticeship as well! 
+      </p>
+    </div>
+  );
+}
