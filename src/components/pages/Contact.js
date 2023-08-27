4a452f855034f04bf89db68aa8c54a62044ee9d5
David Gaspard <dgaspard7777@gmail.com>
2023-08-26 19:46:35 -0500

    Contact page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div className="contactme">
+      <h1>Contact Me</h1>
+      <div id="mygithub" >
+        <a href= "https://www.github.com/Gixxer777">Github</a>
+      </div>
+      <div id="myemail">
+        <a href= "mailto:dgaspard7777@gmail.com">Email</a>
+      </div>
+      <div id="myphone">
+        <a href= "tel:832-870-9946">Phone: (832) 870-9946</a>
+      </div>
+    </div>
+    );
+}
